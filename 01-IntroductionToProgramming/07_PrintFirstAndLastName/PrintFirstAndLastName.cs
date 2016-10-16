76b01beebfaf252a681fe559961babc43881fe07
grigorpavlov <gripavlov@gmail.com>
2016-10-16 21:05:37 +0300

    Add files via upload

---
+++ 01-IntroductionToProgramming/07_PrintFirstAndLastName/PrintFirstAndLastName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintFirstAndLastName
+{
+    class PrintFirstAndLastName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grigor\nPavlov");
+        }
+    }
+}
---
+++ 01-IntroductionToProgramming/08_SqareRoot/SquareRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SqareRoot
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            var a = Math.Sqrt(12345);
+            Console.WriteLine(a);
+        }
+    }
+}
